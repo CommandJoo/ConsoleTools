0dec395d97cd64eb3f52b7f02fffd61217eefe78
Johannes <hansjohannes118@gmail.com>
2023-12-12 12:37:36 +0100

    Added Math Class, changed "exit" to ":q" in math menu, TODO add command system for math

---
+++ src/main/java/de/jo/math/Math.java
@@ -0,0 +1,25 @@
+package de.jo.math;
+
+import org.mariuszgromada.math.mxparser.Constant;
+import org.mariuszgromada.math.mxparser.Function;
+
+import java.util.HashMap;
+
+public class Math {
+
+    private final HashMap<String, Function> functions;
+    private final HashMap<String, Constant> constants;
+
+    public Math() {
+        this.functions = new HashMap<>();
+        this.constants = new HashMap<>();
+    }
+    public HashMap<String, Function> functions() {
+        return functions;
+    }
+
+    public HashMap<String, Constant> constants() {
+        return constants;
+    }
+
+}
--- src/main/java/de/jo/modules/impl/other/ModuleMath.java
+++ src/main/java/de/jo/modules/impl/other/ModuleMath.java
@@ -1,5 +1,7 @@
 package de.jo.modules.impl.other;
 
+import de.jo.Main;
+import de.jo.math.Math;
 import de.jo.modules.Module;
 import de.jo.modules.ModuleInfo;
 import de.jo.util.ConsoleColors;
@@ -19,80 +21,80 @@ import java.util.Scanner;
 @ModuleInfo(name = "math", description = "", syntax = "<Expression>")
 public class ModuleMath implements Module {
 
-    private final HashMap<String, Function> functions = new HashMap<>();
-    private final HashMap<String, Constant> constants = new HashMap<>();
+    private Math math;
 
     @Override
     public void run(String... args) throws Exception {
-        boolean math = true;
+        this.math = new Math();
+        boolean isMath = true;
         boolean verbose = false;
         System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"You're now in Math-Mode!");
         System.out.println(ConsoleColors.YELLOW_BRIGHT+"---------------------------------");
-        System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":f Function\" to write functions");
+        System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":f Function\" to write math.functions()");
         System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":d Function\" to write get a quick derivative function named functionname+\"d\"");
-        System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":c Const\" to write constants");
+        System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":c Const\" to write math.constants()");
         System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":e Expression\" to evaluate expressions");
         System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":v\" to enable/disable calculating documentation");
-        System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \"exit\" to quit");
+        System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"Use the command \":q\" to quit");
         System.out.println(ConsoleColors.YELLOW+"> "+ConsoleColors.YELLOW_BRIGHT+"For more documentation visit https://mathparser.org/mxparser-tutorial/");
         System.out.println(ConsoleColors.YELLOW_BRIGHT+"---------------------------------");
-
-        while(math) {
+        
+        while(isMath) {
             Scanner scanner = new Scanner(System.in);
             String line = "";
             while((line = scanner.nextLine()) != null) {
                 if(line.toLowerCase().startsWith(":f")) {
                     Function function = new Function(line.substring(2));
-                    functions.values().forEach(f -> {
+                    math.functions().values().forEach(f -> {
                         if(!f.getFunctionName().equals(function.getFunctionName())) {
                             function.addDefinitions(f);
                         }
                     });
-                    constants.values().forEach(f -> {
+                    math.constants().values().forEach(f -> {
                         if(!f.getConstantName().equals(function.getFunctionName())) {
                             function.addDefinitions(f);
                         }
                     });
-                    if(!functions.containsKey(function.getFunctionName())) {
-                        functions.put(function.getFunctionName(), function);
+                    if(!math.functions().containsKey(function.getFunctionName())) {
+                        math.functions().put(function.getFunctionName(), function);
                     }else {
-                        functions.replace(function.getFunctionName(), function);
+                        math.functions().replace(function.getFunctionName(), function);
                     }
                     System.out.println(ConsoleColors.GREEN+"> "+function.getFunctionName()+": "+function.getFunctionExpressionString());
                 }
                 else if(line.toLowerCase().startsWith(":d")) {
                     String fun = line.substring(2).trim();
-                    if(!functions.containsKey(fun)) {
+                    if(!math.functions().containsKey(fun)) {
                         Strings.error("Invalid function name: "+fun);
                     }else {
-                        Function function = functions.get(fun);
+                        Function function = math.functions().get(fun);
 
                         Function derivative = new Function(fun+"d", "der("+fun+"(y), y, x)", "x");
-                        functions.values().forEach(f -> {
+                        math.functions().values().forEach(f -> {
                             if(!f.getFunctionName().equals(derivative.getFunctionName())) {
                                 derivative.addDefinitions(f);
                             }
                         });
-                        if(!functions.containsKey(derivative.getFunctionName())) {
-                            functions.put(derivative.getFunctionName(), derivative);
+                        if(!math.functions().containsKey(derivative.getFunctionName())) {
+                            math.functions().put(derivative.getFunctionName(), derivative);
                         }else {
-                            functions.replace(derivative.getFunctionName(), derivative);
+                            math.functions().replace(derivative.getFunctionName(), derivative);
                         }
                         System.out.println(ConsoleColors.GREEN+"> "+derivative.getFunctionName()+": "+derivative.getFunctionExpressionString());
                     }
                 }
                 else if(line.toLowerCase().startsWith(":c")) {
                     Constant cons = new Constant(line.substring(2));
-                    if(!constants.containsKey(cons.getConstantName())) {
-                        constants.put(cons.getConstantName(), cons);
+                    if(!math.constants().containsKey(cons.getConstantName())) {
+                        math.constants().put(cons.getConstantName(), cons);
                     }else {
-                        constants.replace(cons.getConstantName(), cons);
+                        math.constants().replace(cons.getConstantName(), cons);
                     }
                     System.out.println(ConsoleColors.GREEN+"> "+cons.getConstantValue());
                 } else if(line.toLowerCase().startsWith(":e")){
                     Expression expression = new Expression(line.substring(2));
-                    functions.values().forEach(expression::addDefinitions);
-                    constants.values().forEach(expression::addDefinitions);
+                    math.functions().values().forEach(expression::addDefinitions);
+                    math.constants().values().forEach(expression::addDefinitions);
                     expression.enableImpliedMultiplicationMode();
                     if(verbose) expression.setVerboseMode();
                     System.out.println(ConsoleColors.GREEN+"> "+expression.calculate());
@@ -100,10 +102,10 @@ public class ModuleMath implements Module {
                     verbose = !verbose;
                     System.out.println(ConsoleColors.GREEN+"> "+(verbose ? "enabled" : "disabled")+" calculation documentation");
                 }else{
-                    if(line.equals("exit")) {
-                        functions.clear();
-                        constants.clear();;
-                        math = false;
+                    if(line.equals(":q")) {
+                        math.functions().clear();
+                        math.constants().clear();;
+                        isMath = false;
                         break;
                     }else {
                         Strings.error("Invalid operation");
